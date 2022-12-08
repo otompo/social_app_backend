9035067cdaa9d895745a627a60f67e1bf4ec3b80
Abagna <abagnasalifusasco@gmail.com>
2022-12-08 18:22:05 +0000

    latest commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import multer from "multer";
 import donenv from "dotenv";
+import bodyParser from "body-parser";
 
 donenv.config();
 
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
@@ -46,8 +48,20 @@ app.use("/api/likes", likesRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/relationships", relationshipsRoutes);
 
-app.use("/", (req, res) => {
-  res.send("SOCIAL API");
+app.all("/*", function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Access-Token,X-Key"
+  );
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
 });
 
 app.listen(8800, () => {
